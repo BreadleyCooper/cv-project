6703e563624a13b108b8da05e8e43553f992f4d2
Jordan Ulmer <julmer526@gmail.com>
2022-12-01 14:32:42 +0000

    New branch, converted personal.js fo functional

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-import React, { Component } from "react"
+import React from "react"
 import Header from "./components/Header"
 import Personal from "./components/Form/Personal";
 import Skills from "./components/Form/Skills";
 import Education from "./components/Form/Education";
 import Experience from "./components/Form/Experience";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return(
-      <div className="container">
-        <div className="cvContainer">
-        <Header title="CV Creator App" />
-        <Personal title="Personal Information" />
-        <Skills title="Skills" skills=""/>
-        <Education title = "Education" />
-        <Experience title = "Experience" /></div>
-        
-      </div>
+const App = () => {
 
-    
-    )
-  };
-}
+  return(
+    <div className="container">
+      <div className="cvContainer">
+      <Header title="CV Creator App" />
+      <Personal title="Personal Information" />
+      <Skills title="Skills" skills=""/>
+      <Education title = "Education" />
+      <Experience title = "Experience" /></div>
+      
+    </div>
+
+  
+  )
+};
+
 
 
 
 export default App
 
-{/* https://mirza-adnan.github.io/cv-builder/ */}
--- src/components/Form/Personal.js
+++ src/components/Form/Personal.js
@@ -1,75 +1,66 @@
-import React, {Component} from "react";
+import { render } from "@testing-library/react";
+import React, { useState } from "react";
 
-class Personal extends Component {
-    constructor (props) {
-        super(props)
+const Personal = () => {
+    const [details, setDetails] = useState({
+        firstName: "",
+        lastName: "",
+        jobTitle: "",
+        phone: "",
+        email: "",
+        address: "",
+        personalDescription: ""
+    })
+    const [editMode, setEditMode] = useState(true)
 
-        this.state = {
-            firstName: "",
-            lastName: "",
-            jobTitle: "",
-            phone: "",
-            email: "",
-            address: "",
-            personalDescription: "",
-            editMode: true,
-            };
-        
-        this.handleChange = this.handleChange.bind(this)
-    }
-
-    handleChange = (event) => {
+    const handleChange = (event) => {
         const value = event.target.value;
-        this.setState({...this.state, [event.target.name]: value})
+        setDetails(...prevDetails => { return{...prevDetails,[event.target.name]: value}})
     }
 
-    handleTextareaChange = (event) => {
-        this.setState({
-            personalDescription: event.target.value
+    const handleTextareaChange = (event) => {
+        setDetails(prevDetails => {
+            return{
+                ...prevDetails, personalDescription: event.target.value 
+            }
         })
     }
 
-    handleSubmit = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault()
-        if (this.state.editMode === true){
-        this.setState ({
-            editMode: false 
-        })
+        if (editMode === true){
+            setEditMode(false)
         } else {
-            this.setState ({
-                editMode: true
-            })
+            setEditMode(true)
         }
     }
 
-    render() {
-
-        if (this.state.editMode) {
+    if (editMode) {
         return (
             <div className="personalForm">
                 <h2>{this.props.title}</h2>
                 <form>
                     <label htmlFor="first_name">First Name</label>
-                    <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange}  id="first_name" placeholder="First Name"></input>
+                    <input type="text" name="firstName" value={details.firstName} onChange={handleChange}  id="first_name" placeholder="First Name"></input>
 
                     <label htmlFor="last_name">Last Name</label>
-                    <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange}  id="last_name" placeholder="Last Name"></input>
+                    <input type="text" name="lastName" value={details.lastName} onChange={handleChange}  id="last_name" placeholder="Last Name"></input>
 
                     <label htmlFor="job_title">Job Title</label>
-                    <input type="text" name="jobTitle" value={this.state.jobTitle} onChange={this.handleChange}  id="job_title" placeholder="Job Title"></input>
+                    <input type="text" name="jobTitle" value={details.jobTitle} onChange={handleChange}  id="job_title" placeholder="Job Title"></input>
 
                     <label htmlFor="phone">Phone Number</label>
-                    <input type="tel" name="phone" value={this.state.phone} onChange={this.handleChange} id="phone" pattern="[0-9]" placeholder="Phone Number"></input>
+                    <input type="tel" name="phone" value={details.phone} onChange={handleChange} id="phone" pattern="[0-9]" placeholder="Phone Number"></input>
 
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" value={this.state.email} onChange={this.handleChange}  id="first_name" placeholder="Email"></input>
+                    <input type="email" name="email" value={details.email} onChange={handleChange}  id="first_name" placeholder="Email"></input>
 
                     <label htmlFor="address">Address</label>
-                    <input type="text" name="address" value={this.state.address} onChange={this.handleChange}  id="address" placeholder="Address"></input>
+                    <input type="text" name="address" value={details.address} onChange={handleChange}  id="address" placeholder="Address"></input>
 
                     <label htmlFor="description">Personal Description</label>
-                    <textarea type="text" name="description" value={this.state.personalDescription} onChange={this.handleTextareaChange}  id="description" placeholder="Describe yourself here"></textarea>
-                    <button id="eButton" onClick={this.handleSubmit}>Save</button>
+                    <textarea type="text" name="description" value={details.personalDescription} onChange={handleTextareaChange}  id="description" placeholder="Describe yourself here"></textarea>
+                    <button id="eButton" onClick={handleSubmit}>Save</button>
                 </form>
             </div>
         )
@@ -91,6 +82,99 @@ class Personal extends Component {
             )
         }
     }
-}
+
+
+// class Personal extends Component {
+//     constructor (props) {
+//         super(props)
+
+//         this.state = {
+//             firstName: "",
+//             lastName: "",
+//             jobTitle: "",
+//             phone: "",
+//             email: "",
+//             address: "",
+//             personalDescription: "",
+//             editMode: true,
+//             };
+        
+//         this.handleChange = this.handleChange.bind(this)
+//     }
+
+//     handleChange = (event) => {
+//         const value = event.target.value;
+//         this.setState({...this.state, [event.target.name]: value})
+//     }
+
+//     handleTextareaChange = (event) => {
+//         this.setState({
+//             personalDescription: event.target.value
+//         })
+//     }
+
+//     handleSubmit = (event) => {
+//         event.preventDefault()
+//         if (this.state.editMode === true){
+//         this.setState ({
+//             editMode: false 
+//         })
+//         } else {
+//             this.setState ({
+//                 editMode: true
+//             })
+//         }
+//     }
+
+//     render() {
+
+//         if (this.state.editMode) {
+//         return (
+//             <div className="personalForm">
+//                 <h2>{this.props.title}</h2>
+//                 <form>
+//                     <label htmlFor="first_name">First Name</label>
+//                     <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange}  id="first_name" placeholder="First Name"></input>
+
+//                     <label htmlFor="last_name">Last Name</label>
+//                     <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange}  id="last_name" placeholder="Last Name"></input>
+
+//                     <label htmlFor="job_title">Job Title</label>
+//                     <input type="text" name="jobTitle" value={this.state.jobTitle} onChange={this.handleChange}  id="job_title" placeholder="Job Title"></input>
+
+//                     <label htmlFor="phone">Phone Number</label>
+//                     <input type="tel" name="phone" value={this.state.phone} onChange={this.handleChange} id="phone" pattern="[0-9]" placeholder="Phone Number"></input>
+
+//                     <label htmlFor="email">Email</label>
+//                     <input type="email" name="email" value={this.state.email} onChange={this.handleChange}  id="first_name" placeholder="Email"></input>
+
+//                     <label htmlFor="address">Address</label>
+//                     <input type="text" name="address" value={this.state.address} onChange={this.handleChange}  id="address" placeholder="Address"></input>
+
+//                     <label htmlFor="description">Personal Description</label>
+//                     <textarea type="text" name="description" value={this.state.personalDescription} onChange={this.handleTextareaChange}  id="description" placeholder="Describe yourself here"></textarea>
+//                     <button id="eButton" onClick={this.handleSubmit}>Save</button>
+//                 </form>
+//             </div>
+//         )
+//         }
+//         else {
+//             return(
+//                 <div className="personalSaved">
+//                     <div className="nameContainer">
+//                         <p>{this.state.firstName}</p>
+//                         <p>{this.state.lastName}</p>
+//                     </div>
+//                     <p className="address">{this.state.address}</p>
+//                     <p className="jobTitle">{this.state.jobTitle}</p>
+//                     <p className="phone">{this.state.phone}</p>
+//                     <p className="email">{this.state.email}</p>
+//                     <p className="personalDesc">{this.state.personalDescription}</p>
+//                     <button id="editBtn" onClick={this.handleSubmit}>Edit</button>
+//                 </div>
+//             )
+//         }
+//     }
+// }
 
 export default Personal
