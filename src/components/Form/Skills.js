0444ff92d85bf9dc680a384e50ef269fccd01c2c
Jordan Ulmer <julmer526@gmail.com>
2022-12-01 14:49:07 +0000

    updated skills to functional component

--- src/components/Form/Skills.js
+++ src/components/Form/Skills.js
@@ -1,72 +1,136 @@
 import React, {Component} from "react";
 import uniqid from "uniqid"
 
-class Skills extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            skill: {text: "",
-                    id: uniqid()
-                    },
-            skills: []
+const Skills = (props) =>{
+    const [skill, setSkill] = 
+        {text: "",
+        id: uniqid()
         }
+    const [skills, setSkills] = []
 
-        this.handleChange = this.handleChange.bind(this)
-        this.onSubmit = this.onSubmit.bind(this)
-        this.handleDelete = this.handleDelete.bind(this)
-    }
-
-    handleChange = (event) => {
-        this.setState({
-            skill: {
+    const handleChange = (event) => {
+        setSkill({
                 text: event.target.value,
-                id: this.state.skill.id
-            }
+                id: skill.id
         })
     }
 
-    onSubmit = (event) => {
+    const onSubmit = (event) => {
         event.preventDefault()
-        this.setState({
-            skills: this.state.skills.concat(this.state.skill),
-            skill: {
-                text: "",
-                id: uniqid()
-            },
+        setSkills({
+            skills: skills.concat(skill),
+            })
+        setSkill ({
+            text: "",
+            id: uniqid()
         })
     }
 
-    handleDelete = (id) => {
-        this.setState({
-            skills: this.state.skills.filter((skill) =>
+    const handleDelete = (id) => {
+        setSkills({
+            skills: skills.filter((skill) =>
                 skill.id !== id
             )
         })
-        this.render()
     }
 
-    render() {
-        return (
+    return (
+        <div>
+            <h2>{props.title}</h2>
+            <form className="skillForm" onSubmit={onSubmit}>
+                <label htmlFor="skillInput">Skills</label>
+                <input type="text" name="skillInput" value={skill.text} onChange={handleChange} id="skillInput"></input>
+                <button type="submit">Add Skill</button>
+            </form>
             <div>
-                <h2>{this.props.title}</h2>
-                <form className="skillForm" onSubmit={this.onSubmit}>
-                    <label htmlFor="skillInput">Skills</label>
-                    <input type="text" name="skillInput" value={this.state.skill.text} onChange={this.handleChange} id="skillInput"></input>
-                    <button type="submit">Add Skill</button>
-                </form>
-                <div>
-                    <ul>
-                        {this.state.skills.map((skill) => {
-                            return <div className="skillContainer" key={skill.id}><li>{skill.text}</li>
-                            <button id="skillEditBtn" onClick={() => this.handleDelete(skill.id)}>Delete</button>
-                            </div>
-                        })}
-                    </ul>
-                </div>
+                <ul>
+                    {skills.map((skill) => {
+                        return <div className="skillContainer" key={skill.id}><li>{skill.text}</li>
+                        <button id="skillEditBtn" onClick={() => handleDelete(skill.id)}>Delete</button>
+                        </div>
+                    })}
+                </ul>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+
+// class Skills extends Component {
+//     constructor(props) {
+//         super(props)
+
+//         this.state = {
+//             skill: {text: "",
+//                     id: uniqid()
+//                     },
+//             skills: []
+//         }
+
+//         this.handleChange = this.handleChange.bind(this)
+//         this.onSubmit = this.onSubmit.bind(this)
+//         this.handleDelete = this.handleDelete.bind(this)
+//     }
+
+//     handleChange = (event) => {
+//         this.setState({
+//             skill: {
+//                 text: event.target.value,
+//                 id: this.state.skill.id
+//             }
+//         })
+//     }
+
+//         handleDelete = (id) => {
+//         this.setState({
+//             skills: this.state.skills.filter((skill) =>
+//                 skill.id !== id
+//             )
+//         })
+//         this.render()
+//     }
+
+//     onSubmit = (event) => {
+//         event.preventDefault()
+//         this.setState({
+//             skills: this.state.skills.concat(this.state.skill),
+//             skill: {
+//                 text: "",
+//                 id: uniqid()
+//             },
+//         })
+//     }
+
+//     handleDelete = (id) => {
+//         this.setState({
+//             skills: this.state.skills.filter((skill) =>
+//                 skill.id !== id
+//             )
+//         })
+//         this.render()
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h2>{this.props.title}</h2>
+//                 <form className="skillForm" onSubmit={this.onSubmit}>
+//                     <label htmlFor="skillInput">Skills</label>
+//                     <input type="text" name="skillInput" value={this.state.skill.text} onChange={this.handleChange} id="skillInput"></input>
+//                     <button type="submit">Add Skill</button>
+//                 </form>
+//                 <div>
+//                     <ul>
+//                         {this.state.skills.map((skill) => {
+//                             return <div className="skillContainer" key={skill.id}><li>{skill.text}</li>
+//                             <button id="skillEditBtn" onClick={() => this.handleDelete(skill.id)}>Delete</button>
+//                             </div>
+//                         })}
+//                     </ul>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 export default Skills
