de37a3fda081025a6749c90cca2c9f040b5e7907
Jordan Ulmer <julmer526@gmail.com>
2022-12-02 11:11:30 +0000

    converted Header to functional

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,15 @@
 // https://mirza-adnan.github.io/cv-builder/ for reference 
 
-import React, {Component} from "react";
+import React from "react";
 
-class Header extends Component {
-    constructor (props) {
-        super(props)
-    }
-
-    render() {
-        return (
-            <div className="header">
-                <h1>{this.props.title}</h1>
-                <a href="https://github.com/BreadleyCooper">Created by Jordan Ulmer</a>
-            </div>
-        )
-    }
+const Header = (props) => {
+    return(
+        <div className="header">
+        <h1>{this.props.title}</h1>
+        <a href="https://github.com/BreadleyCooper">Created by Jordan Ulmer</a>
+    </div>
+    )
 }
+
 
 export default Header
